062bf6bf929d411bfcd58840e1d0f77132226f67
fabianlaforga21 <90147053+fabianlaforga21@users.noreply.github.com>
2021-11-11 17:40:35 +0100

    Delete basiswebsite directory

--- basiswebsite/scripts/script.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Document
-var deButton = document.querySelector("button");
-
-deButton.addEventListener("click", openMenu);
-
-function openMenu() {
-  document.body.classList.toggle("menuOpen");
-}
